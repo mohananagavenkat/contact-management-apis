e5c9563f0b069bbd21737cc757c364014c9100fb
mohananagavenkat <mohananagavenkat@gmail.com>
2018-11-16 00:18:11 +0530

    added get route for contacts inside it's own file. tried to complete signup api but some error is coming while generating hash using bcrypt. app is also crashing due ti this. But pusing the code since have to work on different lap tommarrowe

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,12 +2,82 @@ const express = require("express");
 
 const router = express.Router();
 
+// requiring user Model to store user data
+const User = require("../models/userModel");
+
+// helper to send error response to client
+const sendError = require("../helpers/sendErrorResponse");
+
+//module to hash passwords
+const bcrypt = require("bcrypt");
+//number of rounds to generate a salt
+const saltRounds = 10;
+
 router.post("/signup", (req, res, next) => {
-    res.send("this is signup route");
+  console.log(req.body);
+  const { firstName, lastName, email, password, confirmPassword } = req.body;
+  if (password !== confirmPassword) {
+    return sendError(res, 401, "password and confirmpassword should be same");
+  }
+  console.log("passowrd and confirm password are same");
+  User.findOne({ email })
+    .then(user => {
+      console.log(user);
+      if (user) {
+        console.log("user already exists");
+        return sendError(res, 401, "The email address already exists");
+      }
+      console.log(password, saltRounds);
+    //   bcrypt.genSalt(saltRounds, function(err, salt) {
+    //       console.log(salt);
+    //     bcrypt.hash(password, salt, function(error, hash) {
+    //       console.log(hash);
+    //     });
+    //   });
+    //   bcrypt.hash(password, saltRounds, function(error, hash) {
+    //     console.log(hash);
+    //     if (error) {
+    //       console.log(error);
+    //       return sendError(res, 401, "something");
+    //     }
+    //     const newUser = new User({
+    //       firstName,
+    //       lastName,
+    //       email,
+    //       password: hash
+    //     });
+    //     newUser
+    //       .save()
+    //       .then(user => {
+    //         console.log("user created");
+    //         console.log(user);
+    //         return res.json({
+    //           status: true,
+    //           message: "User created successfully"
+    //         });
+    //       })
+    //       .catch(error => {
+    //         console.log(error);
+    //         return sendError(res, 401, error);
+    //       });
+    //   });
+      //   bcrypt
+      //     .hash(password, saltRounds)
+      //     .then(hash => {
+
+      //     })
+      //     .catch(error => {
+
+      //     });
+    })
+    .catch(error => {
+      console.log(error);
+      return sendError(res, 401, error);
+    });
 });
 
 router.post("/signin", (req, res, next) => {
-    res.send("this is signin route");
+  res.send("this is signin route");
 });
 
 module.exports = router;
