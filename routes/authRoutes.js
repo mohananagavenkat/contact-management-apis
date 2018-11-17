4b87030eea7ff132f9e5eb8fb7626b380a7e01e9
mohananagavenkat <mohananagavenkat@gmail.com>
2018-11-17 23:25:50 +0530

    developed signup api as well sending an email to the user with activation link

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,14 +4,20 @@ const router = express.Router();
 
 // requiring user Model to store user data
 const User = require("../models/userModel");
+// requiring UserActivationToken Model to store activation token of user in database
+const UserActivationToken = require("../models/userActivationTokenModule");
 
 // helper to send error response to client
 const sendError = require("../helpers/sendErrorResponse");
 
 //module to hash passwords
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 //number of rounds to generate a salt
 const saltRounds = 10;
+// requiring builtin crypto module to generate random token for user activation
+const crypto = require("crypto");
+
+const mailer = require("../helpers/mailer");
 
 router.post("/signup", (req, res, next) => {
   console.log(req.body);
@@ -19,56 +25,44 @@ router.post("/signup", (req, res, next) => {
   if (password !== confirmPassword) {
     return sendError(res, 401, "password and confirmpassword should be same");
   }
-  console.log("passowrd and confirm password are same");
-  User.findOne({ email })
+  User.findOne({ email }) // checking whether email already existed or not
     .then(user => {
-      console.log(user);
       if (user) {
-        console.log("user already exists");
         return sendError(res, 401, "The email address already exists");
       }
-      console.log(password, saltRounds);
-    //   bcrypt.genSalt(saltRounds, function(err, salt) {
-    //       console.log(salt);
-    //     bcrypt.hash(password, salt, function(error, hash) {
-    //       console.log(hash);
-    //     });
-    //   });
-    //   bcrypt.hash(password, saltRounds, function(error, hash) {
-    //     console.log(hash);
-    //     if (error) {
-    //       console.log(error);
-    //       return sendError(res, 401, "something");
-    //     }
-    //     const newUser = new User({
-    //       firstName,
-    //       lastName,
-    //       email,
-    //       password: hash
-    //     });
-    //     newUser
-    //       .save()
-    //       .then(user => {
-    //         console.log("user created");
-    //         console.log(user);
-    //         return res.json({
-    //           status: true,
-    //           message: "User created successfully"
-    //         });
-    //       })
-    //       .catch(error => {
-    //         console.log(error);
-    //         return sendError(res, 401, error);
-    //       });
-    //   });
-      //   bcrypt
-      //     .hash(password, saltRounds)
-      //     .then(hash => {
-
-      //     })
-      //     .catch(error => {
-
-      //     });
+      bcrypt.hash(password, saltRounds, function(error, hash) {
+        if (error) {
+          return sendError(res, 401, "Some Error Occured. Please Try Again After Sometime");
+        }
+        const newUser = new User({
+          firstName,
+          lastName,
+          email,
+          password: hash
+        });
+        newUser
+          .save()
+          .then(user => {
+            const token = crypto.randomBytes(64).toString("hex");
+            const newUserActivationToken = new UserActivationToken({
+              _userId:user.id,
+              token
+            });
+            newUserActivationToken
+              .save()
+              .then(()=>{
+                mailer.send.userActivationEmail(user.email,token);
+                return res.json({
+                  status: true,
+                  message: "User created successfully"
+                });
+              })
+          })
+          .catch(error => {
+            console.log(error);
+            return sendError(res, 401, error);
+          });
+      });
     })
     .catch(error => {
       console.log(error);
@@ -79,5 +73,6 @@ router.post("/signup", (req, res, next) => {
 router.post("/signin", (req, res, next) => {
   res.send("this is signin route");
 });
+
 
 module.exports = router;
