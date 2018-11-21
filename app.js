3a37b4c2e27e0bc8a59552cd4a021c1579e81364
mohananagavenkat <mohananagavenkat@gmail.com>
2018-11-22 00:23:12 +0530

    added options method to allowed methods since it's necessary on CORS

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 // setting access control headers to make CORS possible
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
