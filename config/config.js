42565b40dbf75424abc0db76fd03cc0d95906223
smnvenkat <venkat.sayempu@smactechlabs.com>
2018-11-22 12:12:23 +0530

    moved configuration related stuff to separate config file

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const config  = {
+    mailServer:{
+        host:"smtp.mailtrap.io",
+        userName:"6ad39a1fb16129",
+        password:"c55269bec474ff"
+    },
+    frontEndURL:"http://localhost:3001"
+}
+
+module.exports = config;
