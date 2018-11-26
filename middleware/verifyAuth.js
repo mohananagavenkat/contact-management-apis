0e4eafc6df9ad0317a2a2004aa459d2ff4922c58
mohananagavenkat <mohananagavenkat@gmail.com>
2018-11-26 22:20:45 +0530

    added a new folder called middlewares which contains middleware files and created a middleware called verifyAuth which checks authentication based on token

---
+++ middleware/verifyAuth.js
@@ -0,0 +1,31 @@
+// this file exports a function which verifies the jwtoken
+
+const jwt = require("jsonwebtoken");
+
+//requiring config.js - for jwt secret key
+const config = require("../config/config");
+
+//exporting the actual function
+module.exports = (req,res,next) =>{
+    function sendAuthError(message){
+        return res.status(401).json({
+            status: false,
+            auth: false,
+            message: message
+        });
+    }
+    let token;
+    if(req.headers.authorization){
+        token = req.headers.authorization.split(" ")[1]
+    }else if(req.body.token){
+        token = req.body.token;
+    }else{
+        return sendAuthError("Please send token along with request")
+    }
+    jwt.verify(token, config.jwtSecret, function (err, decoded) {
+        if(err)
+            return sendAuthError(err.message);
+        // decoded undefined
+        next();
+    });
+}
