852e1238f9b15e889ddf582613875fc5e28c2760
mohananagavenkat <mohananagavenkat@gmail.com>
2018-12-03 18:09:01 +0530

    changed contact model added some more fields

--- models/contactModel.js
+++ models/contactModel.js
@@ -3,14 +3,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const contactSchema = new Schema({
+    _userId:String,
     firstName:String,
     lastName:String,
     email:String,
     phoneNumber:String,
-    avatar:String,
     job:String,
     company:String,
-    avatar:String
+    avatar:String,
+    notes:String,
+    createdAt:{ type:Date, default:Date.now },
+    updatedAt:{ type:Date, default:Date.now }
 });
 
 module.exports = mongoose.model("contacts",contactSchema);
